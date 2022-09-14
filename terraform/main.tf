794c9cf100fdbf4212286a21d7757bc876e981be
Daniel Kofler <fox27374@gmail.com>
2022-09-14 18:44:49 +0200

    Added SG attach to instance interface

--- terraform/main.tf
+++ terraform/main.tf
@@ -148,12 +148,17 @@ resource "aws_instance" "bastion" {
   ami           = var.LT["ec2_ami"]
   instance_type = var.LT["ec2_instance_type"]
   subnet_id   = aws_subnet.linux-training-public.id
-  security_groups = [ aws_security_group.linux-training-public.id ]
   key_name = "dkofler-key"
   user_data = "${file("user_data.sh")}"
   tags = {
     Name = var.LT["bastion_name"]
   }
+}
+
+# Attach SG to instance
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = aws_security_group.linux-training-public.id
+  network_interface_id = aws_instance.bastion.primary_network_interface_id
 }
 
 # Create elastic IP
@@ -164,3 +169,4 @@ resource "aws_eip" "linux-training" {
     Name = var.LT["vpc_name"]
   }
 }
+
