fc3eda0e1f3a7523072c0a887698e0bb50e81947
Daniel Kofler <fox27374@gmail.com>
2022-10-03 12:16:34 +0200

    removed local ssh config

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,13 +2,6 @@
 output "bastion-dns-name" {
   value = aws_eip.bastion.public_dns
 }
-output "bastion-public-ip" {
-  value = aws_eip.bastion.public_ip
-}
-
-output "private-ip-addresses" {
-  value = aws_instance.private.*.private_ip
-}
 
 resource "local_file" "ansible_inventory" {
   depends_on = [
